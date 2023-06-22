f04d156db8bf524f230074c68f73c9045d67ecd2
MajorDown <romain.fouillaron@gmx.fr>
2023-06-22 22:54:32 +0200

    premier commit

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./logo192.png" alt="logo react" />{" "}
+      {/*les images doivent être dans public*/}
+      <h3>React World</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        cumque saepe quaerat cum autem necessitatibus assumenda iste vero
+        dolorum debitis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
